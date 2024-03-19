094e636578f13a7f2ca4626675bb61b1a9c90cd7
George <nsengiyumvageorges4@gmail.com>
2024-03-19 20:50:12 +0200

    injecting codes login codes in login.js file.

---
+++ login.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const datas = getSavedDatas()
+
+// Check data exist
+const check = {
+    fullName: '',
+    password: ''
+}
+
+document.querySelector('.login-field').addEventListener('submit', (e) => {
+    e.preventDefault()
+    check.fullName = e.target.elements.fullName.value
+    check.password = e.target.elements.password.value
+    checkData(datas, check)
+})
+console.log(datas)
